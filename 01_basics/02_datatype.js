dba00a3618f960f8b0544e63f3ba3000f7410dfb
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-05 07:20:32 +0000

    DataTypes in js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+"use Strict";//it is use for newer version code
+console.log(typeof null); // it is use to defirne type of value and null is a object type;
+ //primitive data type
+/*
+1.String
+2.Number
+3.Boolean
+4.BigInt
+5.null
+6.undefined
+7.object
+8.Symbol => mostly it is use in react
+
+
+
+*/
+//alert(55) => we can not use alert in node js
