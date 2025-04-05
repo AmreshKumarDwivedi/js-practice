21a55d19935536b198161537875e8c66b9f9cc8f
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-05 07:31:55 +0000

    Conversion and Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+/*
+NAN(Not A Number)
+
+
+number to string 
+     let getNumber = 33;
+     let numberToString = Number(getNumber);
+
+string to number
+     
+ let getstring = "33";
+let stringToNumber = String(getstring);
+
+
+string to boolean or number to boolean
+ 
+ let getData = 1;
+ let getBooleanData = Boolean(getData)
+
+
+
+*/
+ 
+//Operation
+
+console.log(1+2);
+console.log("1"+2+2);
+console.log(2+2+'1');
+
+let countScore = 100;
+countScore++; //postfix or prefix
+console.log(countScore);
