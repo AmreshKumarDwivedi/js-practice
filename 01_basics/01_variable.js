1d6725bf6099832963365b309857bfe1f3ef2298
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-05 07:20:19 +0000

    Variable in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,5 @@
+const studentId = '12344';
+let studentName = 'Amresh'
+// Prefer not to use var beacuse it is not woriking proper in function scope and block scope
+// studentState = 'UP'
+console.table([studentId,studentName]);
