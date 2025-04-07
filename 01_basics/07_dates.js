188bde9fd2b7e99ed7745290e8532b360acd9958
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-07 10:14:37 +0000

    Date and Time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+const date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleString());
+const createdDate = new Date("2002-01-23");
+console.log(createdDate.toLocaleString());
+
+//Time 
+const time = Date.now();
+console.log(date.getDay());
