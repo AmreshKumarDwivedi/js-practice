25ea6be51134ff4243363df3aacc7a796636845a
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-08 12:47:08 +0000

    IIFE Function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+//Immediatly invoked Function Expression(IIFE)
+
+
+(function chai(){      //--> Named IIFE Function
+console.log('Amresh')
+})(); //---> Use semicolon to end this code
+
+
+((name)=>{
+    console.log(`${name} is good boy`)
+
+})('AMresh')
