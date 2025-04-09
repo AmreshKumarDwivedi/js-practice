a34c92f1e6559663292faee88deccc491af84b9d
AmreshKumarDwivedi <113718886+AmreshKumarDwivedi@users.noreply.github.com>
2025-04-09 11:44:20 +0000

    Switch Js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,13 @@
+
+const month = 3;
+switch(month){
+    case 1:
+    console.log('january')
+    break;
+    case 3:
+    console.log('feb')
+    break;
+    case 2:
+    console.log('march')
+    break
+}
